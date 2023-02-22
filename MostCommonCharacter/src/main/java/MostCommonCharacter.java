0240a1cec92d2f6db13a1e55e4f7a34ab686b10d
JoshHampton <Joshhampton56@gmail.com>
2023-02-22 02:13:31 +0000

    Saved progress on MostCommonCharacter 2023-02-22 02:13:31 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,21 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+        HashMap<Character, Integer> highest = new HashMap<>();
+        highest.put(str.charAt(0), 1);
+
+        for (int i = 0; i < str.length(); i++){
+            if (hashmap.containsKey(str.charAt(i))){
+                hashmap.put(str.charAt(i) , hashmap.get(str.charAt(i))+1);
+                if(hashmap.get(str.charAt(i)) > highest){
+                    highest++;
+                }
+            }
+            else{
+                hashmap.put(str.charAt(i), 1);
+            }
+        }
+        return highest.get;
     }
 }
